eca7257150c1c40a1860fd736e0982a8314531fc
Angel Talledo <prueba@correo.com>
2024-07-27 19:16:06 -0500

    Agregar carpetas

---
+++ AppTransporte/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using AppTransporte.Interfaces;
+
+namespace AppTransporte
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Login();
+        }
+    }
+}
---
+++ AppTransporte/Interfaces/Evidencias.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppTransporte.Interfaces;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
