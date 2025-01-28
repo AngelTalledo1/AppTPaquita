ffddd91139bc145bb9138c486fff4e4622f72dc6
Jav Merino <149276417+JavMerino@users.noreply.github.com>
2025-01-28 15:28:06 -0500

    after pull

--- AppTransporte/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppTransporte.AssemblyInfo.cs
+++ AppTransporte/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppTransporte.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTransporte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+20f6a51d76d39ac3c813f2a714babf08377d5cf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7c351f880521bafcf13d9a62b071aa3bb4d5e45c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTransporte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTransporte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
